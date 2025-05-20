b811c23e14754bd5cbcd380dd18411e78ab6b620
bluesmartgit <malli.viz123@gmail.com>
2025-05-20 21:48:52 +0530

    Added consents in validate account and additional document in register user if needed

--- kyc-core/src/main/java/org/techdisqus/request/RegisterUserRequest.java
+++ kyc-core/src/main/java/org/techdisqus/request/RegisterUserRequest.java
@@ -2,11 +2,13 @@ package org.techdisqus.request;
 
 import lombok.Data;
 
+
 import java.util.List;
 
 @Data
 public class RegisterUserRequest extends DigitalOnboardingRequest{
 
     private List<Consent> consents;
+    private List<Document> additionalDocs;
 
 }
--- kyc-core/src/main/java/org/techdisqus/request/ValidateAccountRequest.java
+++ kyc-core/src/main/java/org/techdisqus/request/ValidateAccountRequest.java
@@ -2,8 +2,11 @@ package org.techdisqus.request;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ValidateAccountRequest extends AbstractRequest{
 
     private String accountIdentifier;
+    private List<Consent> consents;
 }
