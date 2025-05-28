773ee9057d6d0f59bd9974daaa673b5b072537ec
bluesmartgit <malli.viz123@gmail.com>
2025-05-28 18:55:14 +0530

    Added error log in exception handler

--- kyc-core/src/main/java/org/techdisqus/exception/GlobalExceptionHandler.java
+++ kyc-core/src/main/java/org/techdisqus/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.techdisqus.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,27 +9,32 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import org.techdisqus.response.AbstractResponse;
 
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<?> handleBadRequest(BadRequestException ex) {
+        log.error("BadRequestException: Error while executing request ", ex);
         return buildResponse(ex, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<?> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        log.error("Error while executing request ", ex);
         return buildResponse(ex, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ApiExecutionException.class)
     public ResponseEntity<?> handleApiExecException(MethodArgumentTypeMismatchException ex) {
+        log.error("ApiExecutionException: Error while executing request ", ex);
         return buildResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGenericException(Exception ex) {
+        log.error("Exception: Error while executing request ", ex);
         return buildResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
